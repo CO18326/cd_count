51233eb373957ccc6a5e7318e90a64599e4df092
CO18326 <42867901+CO18326@users.noreply.github.com>
2019-10-27 23:41:41 +0530

    Add files via upload

---
+++ cd_count.py
@@ -0,0 +1,24 @@
+def cd_count(a,b):
+	
+	if a==0 || b==0:
+		return 2      
+	a=(-1*a if a<0 else a)
+	b=(-1*b if b<0 else b)
+	
+	result=0
+	while a!=0:
+		c=a
+		a=b%a
+		b=c
+	
+	for i in range(1,int((b**0.5)+1)):
+		if b%i==0:
+			if b/i==i:
+				result=result+1
+			else:
+				result=result+2
+	return result
+
+
+
+
